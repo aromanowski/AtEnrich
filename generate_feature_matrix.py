6c676a573515417f6cc6a35a890d09d72bb7b762
Daniel Seaton <daniel.seaton87@gmail.com>
2016-03-24 14:37:20 +0000

    Created generate_feature_matrix function
    generate_feature_matrix is now a callable function. Works
    on a simple testcase of a few genes with known results.

--- generate_feature_matrix.py
+++ generate_feature_matrix.py
@@ -8,28 +8,26 @@ cursor = db.cursor()
 #input gene list
 input_gene_list = ['AT1G09570','AT5G67560', 'AT5G67620', 'AT5G67620']
 
-feature_matrix = []
-
 #annotate these according to presence/absence in gene lists
-gene_list_names = ['phyA_chip_seq']
+gene_list_names = ['chen2014_phyA_chip_seq']
 
 gene_list_name = gene_list_names[0]
-cursor.execute("""SELECT list_id FROM list_info WHERE list_name=%s;""",[gene_list_name])
-result = cursor.fetchall()
-assert(len(result[0])==1)
-gene_list_id = result[0][0]
 
-sql_query = """SELECT locus_id FROM gene_lists WHERE list_id=2;"""
-cursor.execute(sql_query)
-result = cursor.fetchall()
-returned_gene_list = [x[0] for x in result]
+def generate_feature_matrix(input_gene_list,gene_list_names):
+    feature_matrix = []
+    
+    for list_name in gene_list_names:
+        sql_query = """SELECT locus_id FROM gene_lists WHERE list_name=%s;"""
+        cursor.execute(sql_query,list_name)
+        result = cursor.fetchall()
+        returned_gene_list = [x[0] for x in result]
+        
+        #features for this list
+        binary_feature_vector = [int(x in returned_gene_list) for x in input_gene_list]
+        
+        feature_matrix.append(binary_feature_vector)
+    
+    feature_matrix = np.array(feature_matrix)
+    return feature_matrix
 
-#features for this list
-binary_feature_vector = [int(x in returned_gene_list) for x in input_gene_list]
-
-feature_matrix.append(binary_feature_vector)
-
-
-
-
-feature_matrix = np.array(feature_matrix)
+generate_feature_matrix(input_gene_list,gene_list_names)
