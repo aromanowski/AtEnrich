fa0a8b4fc465ce73cdf326ea614233a4817054ce
Daniel Seaton <daniel.seaton87@gmail.com>
2016-03-25 13:15:30 +0000

    Added doctest to convert_clustering...
    Replaced testing scripts with doctest in function description.

--- convert_clustering_to_classification.py
+++ convert_clustering_to_classification.py
@@ -1,20 +1,15 @@
-import pandas as pd
 import numpy as np
-import random
-
-random.seed(0)
-input_cluster_labels = [0,1,2]
-#input clusters
-nG = 8
-cluster_list = [random.choice(input_cluster_labels) for _ in range(nG)]
-
-cluster_labels = sorted(list(set(cluster_list)))
 
 def convert_clustering_to_classification(cluster_list):
+    '''Binarise the input cluster classification list
+    
+    Return a binary numpy array of size (nClusters x nGenes)
+    >>> cluster_list = [0,0,1,1,2,1]
+    >>> convert_clustering_to_classification(cluster_list)
+    array([[1, 1, 0, 0, 0, 0],
+           [0, 0, 1, 1, 0, 1],
+           [0, 0, 0, 0, 1, 0]])
+    '''
     cluster_labels = sorted(list(set(cluster_list)))
     binary_classification = [ [int(x==label) for x in cluster_list] for label in cluster_labels]
     return np.array(binary_classification)
-    
-
-print cluster_list
-print convert_clustering_to_classification(cluster_list)
