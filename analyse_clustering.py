84ec45b54a8cab232af32eaa6370e07ee243ded7
Daniel Seaton <daniel.seaton87@gmail.com>
2016-07-04 16:57:47 +0100

    Converted analyse_clustering into a function that generates a .csv dataframe via pandas

--- analyse_clustering.py
+++ analyse_clustering.py
@@ -8,60 +8,52 @@ import json
 import re
 import sqlite3
 
-###TODO - make into function, and call it from a script
-#def analyse_clustering(file,clusters_to_plot=None,gene_list_names=None,cluster_indices=None):
+def analyse_clustering(clustering_file_location,output_filename,gene_list_names=None,cluster_indices=None):
+    
+    db = sqlite3.connect("GeneListDB.db")
+    cursor = db.cursor()
+    
+    if not gene_list_names:
+        #get default list of gene_list_names i.e. all gene lists
+        cursor.execute("""SELECT list_name FROM list_info;""")
+        gene_list_names = [x[0] for x in cursor.fetchall()]
+        gene_list_names = [x for x in gene_list_names if not re.search('seaton2015',x)]
+    
+    
+    with open(clustering_file_location) as data_file:
+        cluster_data = json.load(data_file)
+    
+    gene_list = cluster_data['gene_list']
+    cluster_list = cluster_data['labels']
+    
+    if not cluster_indices:
+        #use default - analyse all clusters
+        cluster_indices = list(set(cluster_list))
+    
+    #get a binary classification vector for each cluster
+    clustering_classifications = convert_clustering_to_classification(cluster_list)
+    
+    #generate the accompanying feature matrix for all genes in the gene list
+    feature_matrix = generate_feature_matrix(gene_list,gene_list_names)
+    
+    FR_df = pd.DataFrame(index=gene_list_names,columns=cluster_indices)
+    
+    for cluster_idx in cluster_indices:
+        classification = clustering_classifications[cluster_idx]
+        importances,std,indices = rank_features(gene_list,classification,
+                                                feature_matrix,
+                                                n_estimators=800,
+                                                max_features=6,
+                                                max_depth=6)
+        FR_df[cluster_idx] = pd.Series(importances,index=gene_list_names)
+    
+    FR_df.to_csv(output_filename,sep='\t')
+    
+    db.close()
+    
+clustering_file_location = os.getenv('HOME')+'/Dropbox/Work/Circadian/Other projects/PhyA signalling/phya_data_analysis/whole_genome_clustering/'+'diurnal_clustering_090616.json'
+output_filename = 'phyA_clustering_analysis_test.csv'
+gene_list_names = None
+#cluster_indices = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,68]
 
-db = sqlite3.connect("GeneListDB.db")
-cursor = db.cursor()
-
-
-with open(os.getenv('HOME')+'/Dropbox/Work/Circadian/Other projects/PhyA signalling/phya_data_analysis/whole_genome_clustering/'+'diurnal_clustering_090616.json') as data_file:    
-    cluster_data = json.load(data_file)
-
-gene_list = cluster_data['gene_list']
-cluster_list = cluster_data['labels']
-
-
-#get a binary classification vector for each cluster
-clustering_classifications = convert_clustering_to_classification(cluster_list)
-
-#use all available lists
-cursor.execute("""SELECT list_name FROM list_info;""")
-gene_list_names = [x[0] for x in cursor.fetchall()]
-gene_list_names = [x for x in gene_list_names if not re.search('seaton2015',x)]
-
-
-feature_matrix = generate_feature_matrix(gene_list,gene_list_names)
-
-cluster_indices = [5,8,14,68]
-
-FR_df = pd.DataFrame(index=gene_list_names,columns=cluster_indices)
-
-for cluster_idx in cluster_indices:
-    #68 and 5 are phyA and PIFs respectively
-    classification = clustering_classifications[cluster_idx]
-    #for idx,classification in enumerate(clustering_classifications[72:73]):
-    importances,std,indices = rank_features(gene_list,classification,
-                                            feature_matrix,
-                                            n_estimators=800,
-                                            max_features=6,
-                                            max_depth=6)
-    FR_df[cluster_idx] = pd.Series(importances,index=gene_list_names)
-
-FR_df.to_csv('cluster_analysis_test.csv',sep='\t')
-
-
-# Plot the feature importances of the forest
-nF = 12
-FS = 20
-figure()
-
-for idx,cluster_idx in enumerate(cluster_indices):
-    subplot(2,2,idx+1)
-    top_values = FR_df[cluster_idx].nlargest(nF)
-    barh(range(nF),top_values,color="r",align="center")
-    yticks(range(nF), top_values.index)
-    ylim([-1, nF])
-
-
-db.close()
+analyse_clustering(clustering_file_location,output_filename,gene_list_names)
