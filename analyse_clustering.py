00f7cdc1a6413b23692a56967beea8b4e086589c
Daniel Seaton <daniel.seaton87@gmail.com>
2016-07-07 09:44:58 +0100

    Pass db cursor from top level through program, rather than creating separate cursors in multiple places.

--- analyse_clustering.py
+++ analyse_clustering.py
@@ -10,20 +10,22 @@ import sqlite3
 
 def analyse_clustering(clustering_file_location,output_filename,gene_list_names=None,cluster_indices=None):
     
-    db = sqlite3.connect("GeneListDB.db")
+#    db = sqlite3.connect("GeneListDB.db")
+    db = sqlite3.connect("DAPseqDB.db")
     cursor = db.cursor()
     
     if not gene_list_names:
         #get default list of gene_list_names i.e. all gene lists
-        cursor.execute("""SELECT list_name FROM list_info;""")
-        gene_list_names = [x[0] for x in cursor.fetchall()]
-        gene_list_names = [x for x in gene_list_names if not re.search('seaton2015',x)]
+#        cursor.execute("""SELECT list_name FROM list_info;""")
+        cursor.execute("SELECT TF_locus_id FROM TF_TG_associations GROUP BY TF_locus_id;")
+        feature_list = [x[0] for x in cursor.fetchall()]
+#        feature_names = [x for x in feature_list if not re.search('seaton2015',x)]
     
     
     with open(clustering_file_location) as data_file:
         cluster_data = json.load(data_file)
     
-    gene_list = cluster_data['gene_list']
+    genes_of_interest = cluster_data['gene_list']
     cluster_list = cluster_data['labels']
     
     if not cluster_indices:
@@ -34,26 +36,27 @@ def analyse_clustering(clustering_file_location,output_filename,gene_list_names=
     clustering_classifications = convert_clustering_to_classification(cluster_list)
     
     #generate the accompanying feature matrix for all genes in the gene list
-    feature_matrix = generate_feature_matrix(gene_list,gene_list_names)
-    
-    FR_df = pd.DataFrame(index=gene_list_names,columns=cluster_indices)
+#    feature_matrix = generate_feature_matrix(gene_list,gene_list_names)
+    feature_matrix = generate_feature_matrix(genes_of_interest,feature_list,cursor,sql_query="""SELECT TG_locus_id FROM TF_TG_associations WHERE TF_locus_id=?;""")
+        
+    FR_df = pd.DataFrame(index=feature_list,columns=cluster_indices)
     
     for cluster_idx in cluster_indices:
         classification = clustering_classifications[cluster_idx]
-        importances,std,indices = rank_features(gene_list,classification,
+        importances,std,indices = rank_features(genes_of_interest,classification,
                                                 feature_matrix,
-                                                n_estimators=800,
-                                                max_features=6,
-                                                max_depth=6)
-        FR_df[cluster_idx] = pd.Series(importances,index=gene_list_names)
+                                                n_estimators=1200,
+                                                max_features=7,
+                                                max_depth=7)
+        FR_df[cluster_idx] = pd.Series(importances,index=feature_list)
     
     FR_df.to_csv(output_filename,sep='\t')
     
     db.close()
     
 clustering_file_location = os.getenv('HOME')+'/Dropbox/Work/Circadian/Other projects/PhyA signalling/phya_data_analysis/whole_genome_clustering/'+'diurnal_clustering_090616.json'
-output_filename = 'phyA_clustering_analysis_test.csv'
+output_filename = 'DAPseq_GAFER_on_cluster_5_PIF_regulated.csv'
 gene_list_names = None
-#cluster_indices = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,68]
+cluster_indices = [5]
 
-analyse_clustering(clustering_file_location,output_filename,gene_list_names)
+analyse_clustering(clustering_file_location,output_filename,gene_list_names,cluster_indices=cluster_indices)
--- generate_feature_matrix.py
+++ generate_feature_matrix.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import numpy as np
-import re
-import sqlite3
 
-def generate_feature_matrix(genes_of_interest,feature_list,sql_query="""SELECT locus_id FROM gene_lists WHERE list_name=?;""",database_file="GeneListDB.db"):
+def generate_feature_matrix(genes_of_interest,feature_list,db_cursor,sql_query="""SELECT locus_id FROM gene_lists WHERE list_name=?;"""):
     feature_matrix = []
     
     for feature_name in feature_list:
-        returned_gene_list = query_database(sql_query,feature_name,database_file)
+        returned_gene_list = query_database(sql_query,feature_name,db_cursor)
         #features for this list
         binary_feature_vector = [int(x in returned_gene_list) for x in genes_of_interest]
         feature_matrix.append(binary_feature_vector)
@@ -15,11 +13,9 @@ def generate_feature_matrix(genes_of_interest,feature_list,sql_query="""SELECT l
     feature_matrix = np.array(feature_matrix).transpose()
     return feature_matrix
 
-db = sqlite3.connect("GeneListDB.db")
-cursor = db.cursor()
 
-def query_database(sql_query,list_name,database_file):
-    cursor.execute(sql_query,[list_name])
-    result = cursor.fetchall()
+def query_database(sql_query,list_name,db_cursor):
+    db_cursor.execute(sql_query,[list_name])
+    result = db_cursor.fetchall()
     returned_gene_list = [x[0] for x in result]
     return returned_gene_list
