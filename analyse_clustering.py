f2b803b9e026442a37a59ef9d3c4077f97c2e2ea
Daniel D Seaton <daniel.seaton87@gmail.com>
2016-07-30 09:14:59 +0100

    More sql query terms taken as function arguments - implementation now independent of any specific DB file.

--- analyse_clustering.py
+++ analyse_clustering.py
@@ -8,13 +8,14 @@ import json
 import re
 import sqlite3
 
-def analyse_clustering(clustering_file_location,output_filename,cursor,feature_id_column,table_name,gene_list_names=None,cluster_indices=None):
+def analyse_clustering(clustering_file_location,output_filename,cursor,feature_id_column,target_id_column,table_name,gene_list_names=None,cluster_indices=None):
     
     #protect against SQL injection
     def scrub(input_string):
         return ''.join( chr for chr in input_string if (chr.isalnum() or chr=='_'))
     table_name = scrub(table_name)
     feature_id_column = scrub(feature_id_column)
+    target_id_column = scrub(target_id_column)
     
     if not gene_list_names:
         #get default feature list
@@ -36,9 +37,8 @@ def analyse_clustering(clustering_file_location,output_filename,cursor,feature_i
     clustering_classifications = convert_clustering_to_classification(cluster_list)
     
     #generate the accompanying feature matrix for all genes in the gene list
-#    feature_matrix = generate_feature_matrix(genes_of_interest,feature_list,cursor,sql_query="""SELECT locus_id FROM gene_lists WHERE list_name=?;""")
-    feature_matrix = generate_feature_matrix(genes_of_interest,feature_list,cursor,sql_query="""SELECT TG_locus_id FROM TF_TG_associations WHERE TF_locus_id=?;""")
-
+    sql_query = """SELECT {0} FROM {1} WHERE {2}=?;""".format(target_id_column,table_name,feature_id_column)
+    feature_matrix = generate_feature_matrix(genes_of_interest,feature_list,cursor,sql_query=sql_query)
         
     FR_df = pd.DataFrame(index=feature_list,columns=cluster_indices)
     
