2446cd16c5203401c69a5264ace41237e86ebbeb
Daniel Seaton <daniel.seaton87@gmail.com>
2016-10-15 11:16:13 +0100

    Added FE (feature enrichment) as own function, with doc + tests

--- rank_features.py
+++ rank_features.py
@@ -1,8 +1,6 @@
 from sklearn.ensemble import ExtraTreesClassifier
 from scipy.stats import hypergeom
 import numpy as np
-from generate_feature_matrix import generate_feature_matrix
-from matplotlib.pyplot import *
 
 def random_forest(input_gene_list,binary_classification,feature_matrix,n_estimators=400,max_features=None,max_depth=5):
     
@@ -48,3 +46,37 @@ def hypergeometric(binary_classification,feature_matrix):
             FE[feature_idx] = 1.0
     
     return pvals,FE
+    
+def FE(class_vector,feature_matrix):
+    '''Calculate fold enrichment of class members in feature categories
+    
+    Args:
+        class_vector: 1-by-nG array
+        feature_matrix: nG-by-nF array
+    Returns:
+        1-by-nF array
+    
+    Example:
+    >>> class_vector = np.array([0,1,1,0,0,0,0])
+    >>> feature_matrix = np.array([[0,0],[1,0],[1,1],[1,0],[1,0],[0,1],[1,1]])
+    >>> FE(class_vector,feature_matrix)
+    array([ 1.4       ,  1.16666667])'''
+
+    assert(all([x in [0,1] for x in class_vector])) #classification vector is binary
+
+    nF = feature_matrix.shape[1]
+
+    FE = np.ones(nF) #fold enrichment
+
+    M = feature_matrix.shape[0] #total number of objects
+    n = sum(class_vector) #number in class
+    for feature_idx in range(nF):
+        feature_vector = feature_matrix[:,feature_idx]
+        N = sum(feature_vector) #number positive for this feature
+        k = sum(np.multiply(feature_vector,class_vector)) #number of positives within this set
+        try:
+            FE[feature_idx] = (float(k)/n)/(float(N)/M)
+        except ZeroDivisionError:
+            FE[feature_idx] = 1.0
+    
+    return FE
