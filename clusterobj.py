9b5f7efa6ebce6cfda8dc7a321be7186ec967de2
Daniel Seaton <daniel.seaton87@gmail.com>
2016-10-18 09:03:22 +0100

    Made from_json an alternative instantiator of ClusterObj

--- clusterobj.py
+++ clusterobj.py
@@ -3,21 +3,38 @@ import json
 class ClusterObj:
     """A cluster object."""
     
-    def __init__(self,clustering_file_location):
-        self.from_json(clustering_file_location)
+    def __init__(self,data=None):
+        if data is None:
+            keys = [u'datasets',
+            u'description',
+            u'labels',
+            'cluster_gene_lists',
+            u'cluster_method',
+            u'cluster_centers',
+            u'cluster_parameters',
+            u'gene_list',
+            u'label_set',
+            u'cluster_gene_lists']
+            self.data = dict([x,[]] for x in keys)
+        else:
+            self.data = data
+
+    @classmethod
+    def from_json(cls,clustering_file_location):
+        with open(clustering_file_location) as data_file:
+            data = json.load(data_file)
+        
+        data['label_set'] = sorted(list(set(data['labels'])))
+        data['cluster_gene_lists'] = dict([(x,[]) for x in data['label_set']])
+        for label,gene_name in zip(data['labels'],data['gene_list']):
+            data['cluster_gene_lists'][label].append(gene_name)
+        
+        cluster_obj = cls(data)
+        return cluster_obj
     
     def __getitem__(self,key):
         return self.data[key]
-    
-    def from_json(self,clustering_file_location):
-        with open(clustering_file_location) as data_file:
-            self.data = json.load(data_file)
         
-        self.data['label_set'] = sorted(list(set(self['labels'])))
-        self.data['cluster_gene_lists'] = dict([(x,[]) for x in self['label_set']])
-        for label,gene_name in zip(self['labels'],self['gene_list']):
-            self.data['cluster_gene_lists'][label].append(gene_name)
-    
     def get_cluster_label(self,gene_name):
         idx = self['gene_list'].index(gene_name)
         return self['labels'][idx]
