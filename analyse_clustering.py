99520f0e98e3cdbacb5576276be9fd4c6034edbf
Daniel Seaton <daniel.seaton87@gmail.com>
2016-10-31 17:41:20 +0000

    Pass ClusterObj to analyse_clustering #20

--- analyse_clustering.py
+++ analyse_clustering.py
@@ -3,38 +3,34 @@ import pandas as pd
 from convert_clustering_to_classification import convert_clustering_to_classification
 import rank_features
 from generate_feature_matrix import generate_feature_matrix
-import json
 
-def analyse_clustering(clustering_file_location,db_id,method='pval',feature_list=None,excluded_features=None,cluster_indices=None):
+def analyse_clustering(cData,db_id,method='pval',feature_list=None,excluded_features=None,cluster_indices=None):
     """Generate enrichment statistics for a given set of clusters.
     
-    >>> from GAFER import analyse_clustering
+    >>> from GAFER import analyse_clustering,ClusterObj
     >>> import os
     >>> import inspect
     >>> 
     >>> main_dir = os.path.dirname(inspect.getfile(analyse_clustering))
     >>> data_dir = os.path.join(main_dir,'tests','test_data_files')
     >>> clustering_file_location = os.path.join(data_dir,'diurnal_clustering_300916.json')
+    >>> cData = ClusterObj.from_json(clustering_file_location)
     >>> db_id = 'GeneListDB'
     >>> cluster_indices = [85]
     >>> feature_list = ['chen2014_phyA_induced']
     >>> 
     >>> method = 'pval'
-    >>> FR_df = analyse_clustering(clustering_file_location,db_id,method=method,feature_list=feature_list,excluded_features=None,cluster_indices=cluster_indices)
+    >>> FR_df = analyse_clustering(cData,db_id,method=method,feature_list=feature_list,excluded_features=None,cluster_indices=cluster_indices)
     >>> FR_df.loc['chen2014_phyA_induced',85]
     23.982050404665358
     >>> method = 'FE'
-    >>> FR_df = analyse_clustering(clustering_file_location,db_id,method=method,feature_list=feature_list,excluded_features=None,cluster_indices=cluster_indices)
+    >>> FR_df = analyse_clustering(cData,db_id,method=method,feature_list=feature_list,excluded_features=None,cluster_indices=cluster_indices)
     >>> FR_df.loc['chen2014_phyA_induced',85]
     4.0285602503912354
     """
     
-
-    with open(clustering_file_location) as data_file:
-        cluster_data = json.load(data_file)
-    
-    genes_of_interest = cluster_data['gene_list']
-    cluster_list = cluster_data['labels']
+    genes_of_interest = cData['gene_list']
+    cluster_list = cData['labels']
     
     if not cluster_indices:
         #use default - analyse all clusters
--- prototyping_scripts/test_analyse_clustering.py
+++ prototyping_scripts/test_analyse_clustering.py
@@ -1,18 +1,19 @@
-from GAFER import analyse_clustering
+from GAFER import analyse_clustering,ClusterObj
 import os
 import inspect
 
 main_dir = os.path.dirname(inspect.getfile(analyse_clustering))
 data_dir = os.path.join(main_dir,'tests','test_data_files')
 clustering_file_location = os.path.join(data_dir,'diurnal_clustering_300916.json')
+cData = ClusterObj.from_json(clustering_file_location)
 db_id = 'GeneListDB'
 cluster_indices = [85]
 feature_list = ['chen2014_phyA_induced']
 
 method = 'pval'
-FR_df = analyse_clustering(clustering_file_location,db_id,method=method,feature_list=feature_list,excluded_features=None,cluster_indices=cluster_indices)
+FR_df = analyse_clustering(cData,db_id,method=method,feature_list=feature_list,excluded_features=None,cluster_indices=cluster_indices)
 FR_df.loc['chen2014_phyA_induced',85]
 
 method = 'FE'
-FR_df = analyse_clustering(clustering_file_location,db_id,method=method,feature_list=feature_list,excluded_features=None,cluster_indices=cluster_indices)
+FR_df = analyse_clustering(cData,db_id,method=method,feature_list=feature_list,excluded_features=None,cluster_indices=cluster_indices)
 FR_df.loc['chen2014_phyA_induced',85]
