8809152fd25695e8d64071efc3fe574a46d3e56b
Daniel Seaton <daniel.seaton87@gmail.com>
2017-01-29 15:37:04 +0000

    Dummy command line interface using click package

---
+++ atenrich.py
@@ -0,0 +1,12 @@
+import click
+
+@click.command()
+@click.option("--mode",default='list', help="Enrichment mode (list or cluster).")
+
+
+def main(mode):
+    """Dummy function that prints arguments to console."""
+    click.echo("AtEnrich running in {0} mode.".format(mode))
+
+if __name__ == '__main__':
+    main()
