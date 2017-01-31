6b7512ad3148cbd75935533d304b82077b1becfe
Daniel Seaton <daniel.seaton87@gmail.com>
2017-01-31 12:59:25 +0000

    Improved atenrich input parsing

--- atenrich/atenrich.py
+++ atenrich/atenrich.py
@@ -12,19 +12,31 @@ def main(input,output,mode,header):
     """Dummy function that prints arguments to console and copies input to output."""
     click.echo("AtEnrich running in {0} mode.".format(mode))
 
-    if header:
-        header_row = 0
-    else:
-        header_row = None
-    input_df = pd.read_csv(click.format_filename(input),header=header_row,sep='\t')
-
-    cData = dict()
-    cData['gene_list'] = input_df.ix[:,0]
-    cData['labels'] = input_df.ix[:,1]
     db_id = 'GeneListDB'
-    background_gene_list = cData['gene_list']
-    cluster_labels = cData['labels']
-    pval_df,FE_df = calculate_enrichment.calculate_enrichment(cluster_labels,mode,background_gene_list,db_id,cluster_indices=None)
+
+    if header:
+        offset = 1
+    else:
+        offset = 0
+    
+    with open(click.format_filename(input)) as data_file:
+        lines = [x.strip().split('\t') for x in data_file.readlines()[offset:]]
+
+    #First column is always the background gene list    
+    background_gene_list = [x[0] for x in lines]
+
+    if mode=='cluster':
+        #Second column is a list of integer cluster labels
+        cluster_labels = [int(x[1]) for x in lines]
+        pval_df,FE_df = calculate_enrichment.calculate_enrichment(cluster_labels,mode,background_gene_list,db_id,cluster_indices=None)
+    elif mode=='list':
+        #Second column is a subset of gene ids
+        gene_list = [x[1] for x in lines if len(x)==2]
+        assert(len(set(background_gene_list)&set(gene_list))==len(set(gene_list)))
+        pval_df,FE_df = calculate_enrichment.calculate_enrichment(gene_list,mode,background_gene_list,db_id,cluster_indices=None)
+    
+
+    #output to file
     pval_df.to_csv(click.format_filename(output+'_pval.txt'),sep='\t')
     FE_df.to_csv(click.format_filename(output+'_FE.txt'),sep='\t')
 
